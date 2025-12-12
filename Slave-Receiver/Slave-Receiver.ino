47064dfa9149ebdbcfe5c8d3b655826f90973ea7
Uwase Sonia <uwasesonia43@example.com>
2025-12-12 10:36:14 +0200

    first commit

---
+++ Slave-Receiver/Slave-Receiver.ino
@@ -0,0 +1,34 @@
+#include <SoftwareSerial.h>
+SoftwareSerial BT(10, 11); 
+
+char buffer[100]; 
+
+void setup() {
+  Serial.begin(9600);
+  BT.begin(9600);
+  Serial.println("Type your messages:");
+}
+
+void loop() {
+  int i = 0;
+  while (BT.available()) {
+    char c = BT.read();
+    if (c == '\n') break;
+    buffer[i++] = c;
+  }
+  if (i > 0) {
+    buffer[i] = '\0';
+    Serial.print("Message: ");
+    Serial.println(buffer);
+  }
+  i = 0;
+  while (Serial.available()) {
+    char c = Serial.read();
+    if (c == '\n') break;
+    buffer[i++] = c;
+  }
+  if (i > 0) {
+    buffer[i] = '\0';
+    BT.println(buffer);
+  }
+}
